a9de1953ae02afb941947e33079660e9c90f9fa1
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2023-05-30 19:18:18 -0300

    knexfile production sqlite3 set

--- knexfile.js
+++ knexfile.js
@@ -20,22 +20,17 @@ export const test = {
   },
   useNullAsDefault: true
 };
-export const staging = {
-  client: 'postgresql',
+export const production = {
+  client: 'sqlite3',
   connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
+    filename: './src/database/beHero.sqlite'
   },
   migrations: {
-    tableName: 'knex_migrations'
-  }
+    directory: './src/database/migrations'
+  },
 };
-export const production = {
+
+export const staging = {
   client: 'postgresql',
   connection: {
     database: 'my_db',
