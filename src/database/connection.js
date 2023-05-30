2f2dad1f88ab09290d2de39e5a40991298af9f1c
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2023-05-30 20:37:30 -0300

    testando conexão

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ import knex from 'knex';
 import validate from '../utils/validateConfig.js';
 
 const config = validate(process.env);
-
+console.log(config)
 const connection = knex(config);
 
 export default connection;
